a39bd6e463297d9780341a3758387eac0f6675ea
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-04 11:28:48 +0200

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@
 docker rm $(docker ps -aq) --force
 sleep 10
 docker run -d -p 4000:4000 platyna100/wallet-frontend
+docker ps
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker pull platyna100/wallet-frontend
+sleep 1
