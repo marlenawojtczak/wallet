f3e9e57d739b8afbec00feb891df5d0093a06f76
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-10-07 00:52:30 +0200

    Added Modal

--- frontend/src/components/HomeTab/HomeTab.styled.jsx
+++ frontend/src/components/HomeTab/HomeTab.styled.jsx
@@ -26,3 +26,24 @@ export const Home = styled.div`
   right: 16px;
   top: 126px;
 `;
+
+export const Wrapper = styled.div`
+  @media screen and (max-width: 768px) {
+    height: 568px;
+    border: 2px solid black;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+  @media screen and (min-width: 769px) and (max-width: 1279px) {
+    height: 1024px;
+    border: 2px solid orange;
+    margin: 0 auto;
+  }
+  @media screen and (min-width: 1280px) {
+    height: 720px;
+    border: 2px solid blue;
+    margin: 0 auto;
+  }
+`;
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,38 +1,39 @@
 import { Helmet } from "react-helmet";
+import Media from "react-media";
+import { useState } from "react";
+import img from "../../assets/temporary/homepage.jpg";
+import { Wrapper } from "../../components/HomeTab/HomeTab.styled";
+import { Balance } from "../../components/Balance";
+import { Currency } from "../../components/Currency";
 import { HomeTab } from "../../components/HomeTab";
 import { ButtonAddTransactions } from "../../components/ButtonAddTransactions";
-import { Balance } from "../../components/Balance";
-import { Wrapper } from "./HomePage.styled";
-import img from "../../assets/temporary/homepage.jpg";
-
-const handleClick = () => {
-  console.log("Add transaction");
-};
+import { ModalAddTransaction } from "../../components/ModalAddTransaction";
 
 const HomePage = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
   return (
     <>
-      <Helmet>
-        <title>Home</title>
-      </Helmet>
-      {/* HERE PUT YOUR COMPONENTS  */}
       <Wrapper>
+        <Helmet>
+          <title>Home</title>
+        </Helmet>
         <Balance />
-        <HomeTab />
-        <ButtonAddTransactions onClick={handleClick} />
-      </Wrapper>
+        <Media query={{ maxWidth: 767 }}>
+          {(matches) => (matches ? <></> : <Currency />)}
+        </Media>
 
-      {/* YOU CAN DELETE CODE BELOW LATER */}
-      <ul>
-        This is <b>Home Page</b>. Please import and use here <b>componets </b>
-        like:
-        <hr />
-        <li>Navigation</li>
-        <li>Balance</li>
-        <li>Currency</li>
-        <li>HomeTab</li>
-        <li>ButtonAddTransactions</li>
-      </ul>
+        <HomeTab />
+        <ButtonAddTransactions onClick={handleOpenModal} />
+        <ModalAddTransaction isOpen={modalOpen} onClose={handleCloseModal} />
+      </Wrapper>
 
       <img src={img} alt="Homepage" />
     </>
