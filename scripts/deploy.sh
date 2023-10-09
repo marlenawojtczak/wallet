5c91281f313435a8ed604ecc59babab2fd58e512
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-09 20:19:20 +0200

    dockerize backend & add to stack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker stack rm wallet
-docker stack deploy -c docker-stack.yml wallet --prune --with-registry-auth
+docker stack deploy -c docker-stack.yaml wallet --prune --with-registry-auth
