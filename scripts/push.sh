5244fc933ec4aaca97f813b52661db1993168bf5
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-09 20:08:14 +0200

    dockerize backend & add to stack

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,7 +4,7 @@ for folder in services/*; do
     if [[ -f ${folder}/Dockerfile ]]; then
         container=$(basename "${folder}")
 
-        docker tag "${container}" alframi410/"${container}":stack
-        docker push alframi410/"${container}":stack
+        docker tag "${container}" platyna100/"${container}":stack
+        docker push platyna100/"${container}":stack
     fi
 done
