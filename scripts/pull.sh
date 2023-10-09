d13560456f6bc71d7c6fce6a45d4df9a863e55cb
Marlena Wojtczak <117003410+marlenawojtczak@users.noreply.github.com>
2023-10-09 23:05:07 +0200

    Update pull.sh

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for image in $(cat docker-stack.yml | envsubst | grep "image:" | awk '{ print $2 }'); do
+for image in $(cat docker-stack.yaml | envsubst | grep "image:" | awk '{ print $2 }'); do
     echo "Pull image $image"
     docker pull ${image}
 done
