01d60095aa8702d6340ed4310db81217af1c1eb7
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-09 20:47:35 +0200

    change baseURL

--- frontend/src/redux/session/operations.js
+++ frontend/src/redux/session/operations.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://172.17.0.1:3000",
 });
 
 const setAuthHeader = (token) => {
