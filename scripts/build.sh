ec9508c665770374eaa65e33cf1ea2672e55a676
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-09 20:35:57 +0200

    dockerize backend & add to stack

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ for folder in services/*; do
     if [[ -f ${folder}/Dockerfile ]]; then
         container=$(basename "${folder}")
 
-        docker build -t "${container}" -f "${folder}"/Dockerfile .
+        docker build -t wallet-"${container}" -f "${folder}"/Dockerfile .
     fi
 done
--- scripts/push.sh
+++ scripts/push.sh
@@ -4,7 +4,7 @@ for folder in services/*; do
     if [[ -f ${folder}/Dockerfile ]]; then
         container=$(basename "${folder}")
 
-        docker tag wallet_"${container}" platyna100/wallet_"${container}":stack
-        docker push platyna100/wallet_"${container}":stack
+        docker tag wallet-"${container}" platyna100/wallet-"${container}":stack
+        docker push platyna100/wallet-"${container}":stack
     fi
 done
