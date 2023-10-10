9cbbc7c4c5e9f5b8ef71db1c4d30b136953abff9
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-10-10 20:46:06 +0200

    edited respones and docs

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,11 +1,13 @@
 export const currentUser = (req, res, next) => {
   const user = req.user;
   return res.status(200).send({
-    id: user?._id,
-    username: user?.username,
-    email: user?.email,
-    accessToken: user?.accessToken,
-    refreshToken: user?.refreshToken,
-    balance: user?.balance,
+    user: {
+      id: user?._id,
+      username: user?.username,
+      email: user?.email,
+      accessToken: user?.accessToken,
+      refreshToken: user?.refreshToken,
+      balance: user?.balance,
+    },
   });
 };
