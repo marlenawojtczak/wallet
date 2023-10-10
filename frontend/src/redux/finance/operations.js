6e4235e8d75f01f666e91c1853001342e996a410
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-10-10 22:00:55 +0200

    fixed user reducer

--- frontend/src/redux/finance/operations.js
+++ frontend/src/redux/finance/operations.js
@@ -1,1 +1,18 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://wallet.dupawklamerkach.pl",
+});
+
+export const fetchTotals = createAsyncThunk(
+  "transactions/fetchTotals",
+  async (_, thunkAPI) => {
+    try {
+      const response = await api.get("/api/transactions/categories");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
