348864d913ad8f7f625d9954ca2281bc26ad8423
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-10-12 18:39:16 +0200

    added dark-mode

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { SharedLayout, AccessLayout } from "../components";
 import { PrivateRoute } from "../routes/PrivateRoute";
 import { RestrictedRoute } from "../routes/RestrictedRoute";
 import { useAuth } from "../hooks/useAuth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../components/darkMode/globalStyles";
+import { lightTheme, darkTheme } from "../components/darkMode/Themes";
+import { useDarkMode } from "../components/darkMode/useDarkMode";
+import Toggle from "./darkMode/Toggler";
 // import { useSelector } from "react-redux";
 // import { ModalLogout } from "./ModalLogout";
 // import { ModalAddTransaction } from "./ModalAddTransaction";
@@ -22,54 +27,63 @@ const Statistics = lazy(() => import("../pages/StatisticsPage/StatisticsPage"));
 const Currency = lazy(() => import("../pages/CurrencyPage/CurrencyPage"));
 
 export const App = () => {
-  const { getCurrentUser } = useAuth();
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
 
+  const { getCurrentUser } = useAuth();
   useEffect(() => {
     getCurrentUser();
   }, [getCurrentUser]);
 
+  if (!mountedComponent) return <div />;
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<AccessLayout />}>
-            <Route
-              path="/register"
-              element={
-                <RestrictedRoute component={Register} redirectTo="/login" />
-              }
-            />
-            <Route
-              path="/login"
-              element={<RestrictedRoute component={Login} redirectTo="/home" />}
-            />
-          </Route>
-          <Route element={<SharedLayout />}>
-            <Route
-              path="/home"
-              element={<PrivateRoute component={Home} redirectTo="/login" />}
-            />
-            <Route
-              path="/statistics"
-              element={
-                <PrivateRoute component={Statistics} redirectTo="/login" />
-              }
-            />
-            <Route
-              path="/currency"
-              element={
-                <PrivateRoute component={Currency} redirectTo="/login" />
-              }
-            />
-          </Route>
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-        <ToastContainer
-          position="top-center"
-          autoClose={2500}
-          pauseOnHover={false}
-        />
-      </Suspense>
-    </>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<AccessLayout />}>
+              <Route
+                path="/register"
+                element={
+                  <RestrictedRoute component={Register} redirectTo="/login" />
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <RestrictedRoute component={Login} redirectTo="/home" />
+                }
+              />
+            </Route>
+            <Route element={<SharedLayout />}>
+              <Route
+                path="/home"
+                element={<PrivateRoute component={Home} redirectTo="/login" />}
+              />
+              <Route
+                path="/statistics"
+                element={
+                  <PrivateRoute component={Statistics} redirectTo="/login" />
+                }
+              />
+              <Route
+                path="/currency"
+                element={
+                  <PrivateRoute component={Currency} redirectTo="/login" />
+                }
+              />
+            </Route>
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+          <ToastContainer
+            position="top-center"
+            autoClose={2500}
+            pauseOnHover={false}
+          />
+        </Suspense>
+      </>
+    </ThemeProvider>
   );
 };
