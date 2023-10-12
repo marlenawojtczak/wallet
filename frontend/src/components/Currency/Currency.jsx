97b6acb445e5f2392a8e47169c0f71351571fb24
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-10-12 17:19:54 +0200

    adding functionality to currency component

--- frontend/src/components/Currency/Currency.jsx
+++ frontend/src/components/Currency/Currency.jsx
@@ -6,28 +6,64 @@ import {
   TableBody,
   TableNextRows,
 } from "./Currency.styled";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const currency_API_URL = "http://api.nbp.pl/api/exchangerates/rates/c/";
+
+async function fetchCurrency(currencyCode) {
+  try {
+    const result = await axios.get(`${currency_API_URL}${currencyCode}`);
+    const data = await result.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 export const Currency = () => {
+  const [exchangeRate, setExchangeRate] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //loader()
+      try {
+        const currenciesToFetch = ["EUR", "USD"];
+        const currencyPromises = currenciesToFetch.map((currencyCode) =>
+          fetchCurrency(currencyCode)
+        );
+        const currencyData = await Promise.all(currencyPromises);
+        console.log("currencyData", currencyData);
+        // removeLoader()
+
+        setExchangeRate(currencyData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <TableWrapper>
       <TableContainer>
         <Table>
           <TableHead>
+            {/* <TableNextRows> */}
             <th>Currency</th>
             <th>Purchase</th>
             <th>Sale</th>
+            {/* </TableNextRows> */}
           </TableHead>
           <TableBody>
-            <TableNextRows>
-              <td>USD</td>
-              <td>27.55</td>
-              <td>27.65</td>
-            </TableNextRows>
-            <TableNextRows>
-              <td>EUR</td>
-              <td>30.00</td>
-              <td>30.10</td>
-            </TableNextRows>
+            {exchangeRate.map((item, index) => (
+              <TableNextRows key={index}>
+                <td>{item.code}</td>
+                <td>{item.rates[0].ask.toFixed(2)}</td>
+                <td>{item.rates[0].bid.toFixed(2)}</td>
+              </TableNextRows>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
