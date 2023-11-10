cd725ac2eefa6fe692103a1e8c91338345d9e8bd
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-11-10 16:51:51 +0100

    added footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { FooterBtn } from "./Footer.styled";
+
+export const Footer = () => {
+  return (
+    <FooterBtn>
+      <span>^</span>
+      <span>© 2023 | All Rights Reserved | Developed by Clamers</span>
+    </FooterBtn>
+  );
+};
