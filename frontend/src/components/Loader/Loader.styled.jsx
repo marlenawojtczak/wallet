6dfe489162698b23e6432d59e804316b11830d2f
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-11-14 20:08:02 +0100

    update loader

--- frontend/src/components/Loader/Loader.styled.jsx
+++ frontend/src/components/Loader/Loader.styled.jsx
@@ -1,70 +1,102 @@
 import styled, { keyframes } from "styled-components";
 
-const loaderspin = keyframes`
+const animloader = keyframes`
   0% {
-    transform: translate(-50%, -50%) rotate(0deg);
+    left: 0;
+    transform: translateX(-100%);
   }
   100% {
-    transform: translate(-50%, -50%) rotate(360deg);
+    left: 100%;
+    transform: translateX(0%);
   }
-`;
-
-const loaderpulse = keyframes`
-  0% {
-    transform: scale(2.5);
-  }
-  100% {
-    transform: scale(2.5);
-  }
-`;
-
-export const LoaderContainer = styled.div`
-  /* background-color: var(--bg-modal-overlay); */
-  background-color: transparent;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-  margin-top: 0px;
-  top: 0px;
-  z-index: 9999;
 `;
 
 export const LoaderContent = styled.div`
+  width: 100%;
+  height: 5px;
+  display: inline-block;
+  background: transparent;
+  overflow: hidden;
   position: absolute;
-  content: "";
-  display: block;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 170px;
-  height: 170px;
-  border-top: 6px solid var(--brand-primary);
-  border-radius: 50%;
-  animation: ${loaderspin} 1.8s infinite ease-in-out;
 
-  &:before {
-    position: absolute;
+  &::after {
     content: "";
-    display: block;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 150px;
-    height: 150px;
-    border-top: 6px solid var(--brand-secondary);
-    border-radius: 50%;
-    animation: ${loaderspin} 1.8s infinite ease-in-out;
+    box-sizing: border-box;
+    width: 192px;
+    height: 5px;
+    background: var(--brand-primary);
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: ${animloader} 900ms linear infinite;
   }
 `;
 
-export const LoaderIcon = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-`;
+// const loaderspin = keyframes`
+//   0% {
+//     transform: translate(-50%, -50%) rotate(0deg);
+//   }
+//   100% {
+//     transform: translate(-50%, -50%) rotate(360deg);
+//   }
+// `;
 
-export const LoaderImage = styled.div`
-  animation: ${loaderpulse} alternate 900ms infinite;
-`;
+// const loaderpulse = keyframes`
+//   0% {
+//     transform: scale(2.5);
+//   }
+//   100% {
+//     transform: scale(2.5);
+//   }
+// `;
+
+// export const LoaderContainer = styled.div`
+//   /* background-color: var(--bg-modal-overlay); */
+//   background-color: transparent;
+//   height: 100%;
+//   width: 100%;
+//   position: fixed;
+//   margin-top: 0px;
+//   top: 0px;
+//   z-index: 9999;
+// `;
+
+// export const LoaderContent = styled.div`
+//   position: absolute;
+//   content: "";
+//   display: block;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   width: 170px;
+//   height: 170px;
+//   border-top: 6px solid var(--brand-primary);
+//   border-radius: 50%;
+//   animation: ${loaderspin} 1.8s infinite ease-in-out;
+
+//   &:before {
+//     position: absolute;
+//     content: "";
+//     display: block;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//     width: 150px;
+//     height: 150px;
+//     border-top: 6px solid var(--brand-secondary);
+//     border-radius: 50%;
+//     animation: ${loaderspin} 1.8s infinite ease-in-out;
+//   }
+// `;
+
+// export const LoaderIcon = styled.div`
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   text-align: center;
+// `;
+
+// export const LoaderImage = styled.div`
+//   animation: ${loaderpulse} alternate 900ms infinite;
+// `;
