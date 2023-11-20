c037cbd6a284503fd8a1060d2d3aededa1dc3562
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-11-20 15:18:23 +0100

    fix/lis/loaderShow
    setTimeout added

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
 import { LoaderContent } from "./Loader.styled";
 
 export const Loader = () => {
-  return <LoaderContent />;
+  const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowLoader(true);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return showLoader ? <LoaderContent /> : null;
 };
+
+// import { LoaderContent } from "./Loader.styled";
+
+// export const Loader = () => {
+//   return <LoaderContent />;
+// };
 
 // import {
 //   LoaderContainer,
